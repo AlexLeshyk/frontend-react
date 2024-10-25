627a78b48e2867897d836e46999f8528ee845475
Gashy_jet <gashy_jet@gmail.com>
2024-10-25 14:03:24 +0300

    feat: add error boundary

---
+++ src/widgets/ErrorStub/ui/ErrorStub.tsx
@@ -0,0 +1,26 @@
+import cx from 'clsx';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui/Button/Button';
+import classes from './ErrorStub.module.css';
+
+interface ErrorPageProps {
+    className?: string;
+}
+
+export const ErrorStub = ({ className }: ErrorPageProps) => {
+  const { t } = useTranslation();
+
+  const reloadPage = () => {
+    // eslint-disable-next-line no-restricted-globals
+    location.reload();
+  };
+
+  return (
+    <div className={cx({ [classes.pageError]: true, [className]: className })}>
+      <p>{t('Unexpected error')}</p>
+      <Button onClick={reloadPage}>
+        {t('Reload page')}
+      </Button>
+    </div>
+  );
+};
