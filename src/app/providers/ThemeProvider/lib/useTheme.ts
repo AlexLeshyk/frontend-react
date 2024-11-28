35201bc5068085710fd931b4f9a96ec68a7be7ff
Gashy_jet <gashy_jet@gmail.com>
2024-11-28 12:16:46 +0300

    feat: add orange theme

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -10,7 +10,19 @@ export const useTheme = (): useThemeResult => {
   const { theme, setTheme } = useContext(ThemeContext);
 
   const changeTheme = () => {
-    const activeTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+    let activeTheme: Theme;
+    switch (theme) {
+      case Theme.DARK:
+        activeTheme = Theme.LIGHT;
+        break;
+      case Theme.LIGHT:
+        activeTheme = Theme.DARK;
+        break;
+      // case Theme.ORANGE:
+      //   activeTheme = Theme.DARK;
+      //   break;
+      default: activeTheme = Theme.LIGHT;
+    }
     setTheme?.(activeTheme);
     document.body.className = activeTheme;
     localStorage.setItem(LOCAL_STORAGE_THEME, activeTheme);
