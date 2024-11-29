56f937f78b43ee6400d0ce14ba32e469c296d725
Gashy_jet <gashy_jet@gmail.com>
2024-11-29 14:26:38 +0300

    feat: udpate npm packages

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -45,6 +45,7 @@ export default {
   moduleNameMapper: {
     '\\.(css|scss)$': 'identity-obj-proxy',
     '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+    axios: 'axios/dist/node/axios.cjs',
   },
   globals: {
     __IS_DEV__: true,
